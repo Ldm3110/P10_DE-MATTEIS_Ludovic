a2763249869d62f3af2d88634e07655142a80940
Ldm3110 <ludovic.dematteis@icloud.com>
2021-11-29 15:25:38 +0100

    creates utilities folder

---
+++ SoftDesk_API/utilities.py
@@ -0,0 +1,24 @@
+TAG = [
+    ('BUG', 'BUG'),
+    ('TACHE', 'TACHE'),
+    ('AMELIORATION', 'AMELIORATION')
+]
+
+PRIORITY = [
+    ('FAIBLE', 'FAIBLE'),
+    ('MOYENNE', 'MOYENNE'),
+    ('ELEVEE', 'ELEVEE',)
+]
+
+STATUS = [
+    ('A FAIRE', 'A FAIRE'),
+    ('EN COURS', 'EN COURS'),
+    ('TERMINE', 'TERMINE'),
+]
+
+TYPE = [
+    ('BACK-END', 'BACK-END'),
+    ('FRONT-END', 'FRONT-END'),
+    ('iOS', 'iOS'),
+    ('ANDROID', 'ANDROID')
+]
