2b4e875adcec10fb4973dadcbbc29225d642ad3f
Ldm3110 <ludovic.dematteis@icloud.com>
2021-12-01 11:54:07 +0100

    creates routers for issues and contributors

--- SoftDesk_API/config/urls.py
+++ SoftDesk_API/config/urls.py
@@ -15,11 +15,37 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_nested import routers
+
+from IssueTrackingSystem.views import ProjectViewSet, IssuesViewSet, ContributorViewSet
+
+router = routers.SimpleRouter()
+router.register(r'projects', ProjectViewSet, basename='projects')
+# --- generates :
+# /projects/
+# /projects/{project_pk}
+
+project_issue = routers.NestedSimpleRouter(router, r'projects', lookup='project')
+project_issue.register(r'issues', IssuesViewSet, basename='project-issue')
+# ---generates :
+# /projects/{project_pk}/issues
+# /projects/{project_pk}/issues/{issue_pk}
+
+contributors_of_project = routers.NestedSimpleRouter(router, r'projects', lookup='project')
+contributors_of_project.register(r'users', ContributorViewSet, basename='contrib-of-project')
+# --- generates :
+# /projects/{project_pk}/users
+# /projects/{project_pk}/users/{user_pk}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Registration path, auth with token and refresh token
     path('', include('authentication.urls')),
     # Projects, Issues and Comments
+    path('', include(router.urls)),
+    path('', include(project_issue.urls)),
+
+    # Add, display or delete a user in a project
+    path('', include(contributors_of_project.urls)),
 
 ]
