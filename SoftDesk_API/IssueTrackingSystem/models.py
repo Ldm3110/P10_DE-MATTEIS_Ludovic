2cbc0221e27892d1cde60e7c6f4112e28a78129f
Ldm3110 <ludovic.dematteis@icloud.com>
2021-12-06 11:58:27 +0100

    modification in models.py

--- SoftDesk_API/IssueTrackingSystem/models.py
+++ SoftDesk_API/IssueTrackingSystem/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from authentication.models import User
+from config import settings
 from utilities import *
 
 
@@ -12,33 +13,34 @@ class Projects(models.Model):
     contribution = models.ManyToManyField(User, through='Contributors')
 
     def __str__(self):
-        return self.title
+        return "%s %s" % (self.id, self.title)
 
 
 class Issues(models.Model):
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.TextField(max_length=4000)
     tag = models.CharField(max_length=128, choices=TAG)
     priority = models.CharField(max_length=128, choices=PRIORITY)
     status = models.CharField(max_length=128, choices=STATUS)
 
     project_id = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    author_user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return "#%s %s" % (self.id, self.title)
 
 
 class Comments(models.Model):
     description = models.TextField(max_length=4000)
 
     issues_id = models.ForeignKey(Issues, on_delete=models.CASCADE)
-    author_user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    author_user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.description
 
 
 class Contributors(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     project_id = models.ForeignKey(Projects, on_delete=models.CASCADE)
     role = models.CharField(max_length=255, choices=ROLE)
