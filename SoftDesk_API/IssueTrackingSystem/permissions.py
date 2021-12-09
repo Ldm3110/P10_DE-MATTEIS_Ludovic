6a0814ab8ffd029449bb71040cab02ac0e68068d
Ldm3110 <ludovic.dematteis@icloud.com>
2021-12-09 15:02:56 +0100

    Creates permissions for issues and comments

--- SoftDesk_API/IssueTrackingSystem/permissions.py
+++ SoftDesk_API/IssueTrackingSystem/permissions.py
@@ -13,7 +13,7 @@ class IsProjectCreatorOrContributor(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method == "GET":
             try:
-                contribution = request.user.contributors_set.get(project_id=obj)
+                contribution = request.user.contributors_set.get(project_id=obj.project_id)
                 if contribution.role == "CREATEUR" or contribution.role == "CONTRIBUTEUR":
                     return True
             except Contributors.DoesNotExist:
@@ -21,8 +21,28 @@ class IsProjectCreatorOrContributor(BasePermission):
 
         elif request.method in {"PUT", "DELETE"}:
             try:
-                contribution = request.user.contributors_set.get(project_id=obj)
+                contribution = request.user.contributors_set.get(project_id=obj.project_id)
                 if contribution.role == "CREATEUR":
                     return True
             except Contributors.DoesNotExist:
                 return False
+
+
+class IsIssueOrCommentAuthor(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.method == "GET":
+            try:
+                contribution = request.user.contributors_set.get(project_id=obj.project_id)
+                if contribution.role == "CREATEUR" or contribution.role == "CONTRIBUTEUR":
+                    return True
+            except Contributors.DoesNotExist:
+                return False
+
+        elif request.method in {"PUT", "DELETE"}:
+            return request.user == obj.author_user_id
