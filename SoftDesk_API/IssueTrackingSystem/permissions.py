1a0048bea1c356700bfc6d7c9ef95d6fcaf090e1
Ldm3110 <ludovic.dematteis@icloud.com>
2021-12-13 14:27:45 +0100

    Creates a new permission (IsCommentAuthor) + some operational changes made

--- SoftDesk_API/IssueTrackingSystem/permissions.py
+++ SoftDesk_API/IssueTrackingSystem/permissions.py
@@ -1,9 +1,34 @@
 from rest_framework.permissions import BasePermission
 
-from IssueTrackingSystem.models import Contributors
+from IssueTrackingSystem.models import Contributors, Issues
 
 
 class IsProjectCreatorOrContributor(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.method == "GET":
+            try:
+                contribution = request.user.contributors_set.get(project_id=obj.id)
+                if contribution.role == "CREATEUR" or contribution.role == "CONTRIBUTEUR":
+                    return True
+            except Contributors.DoesNotExist:
+                return False
+
+        elif request.method in {"PUT", "DELETE"}:
+            try:
+                contribution = request.user.contributors_set.get(project_id=obj.id)
+                if contribution.role == "CREATEUR":
+                    return True
+            except Contributors.DoesNotExist:
+                return False
+
+
+class IsIssueAuthor(BasePermission):
 
     def has_permission(self, request, view):
         if request.user.is_authenticated:
@@ -20,15 +45,10 @@ class IsProjectCreatorOrContributor(BasePermission):
                 return False
 
         elif request.method in {"PUT", "DELETE"}:
-            try:
-                contribution = request.user.contributors_set.get(project_id=obj.project_id)
-                if contribution.role == "CREATEUR":
-                    return True
-            except Contributors.DoesNotExist:
-                return False
+            return request.user == obj.author_user_id
 
 
-class IsIssueOrCommentAuthor(BasePermission):
+class IsCommentAuthor(BasePermission):
 
     def has_permission(self, request, view):
         if request.user.is_authenticated:
@@ -38,7 +58,8 @@ class IsIssueOrCommentAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method == "GET":
             try:
-                contribution = request.user.contributors_set.get(project_id=obj.project_id)
+                issue = Issues.objects.get(id=obj.issues_id_id)
+                contribution = request.user.contributors_set.get(project_id=issue.project_id)
                 if contribution.role == "CREATEUR" or contribution.role == "CONTRIBUTEUR":
                     return True
             except Contributors.DoesNotExist:
